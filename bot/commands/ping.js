bf1ef0972c1510d54b5fd3f83b2639c00f6de5c1
Notspay <148631539+Sander512@users.noreply.github.com>
2026-09-21 22:45:20 +0200

    Delete bot directory

--- bot/commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    async execute(interaction) {
-        const sent = await interaction.reply({ content: 'Pingen...', fetchReply: true });
-        const latency = sent.createdTimestamp - interaction.createdTimestamp;
-        await interaction.editReply(`Pong! Botlatency: ${latency}ms. API-latency: ${Math.round(interaction.client.ws.ping)}ms.`);
-    },
-};
